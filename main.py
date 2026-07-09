c1737525271c0bc0282cbc59799b400e13d0460e
Koos85 <koosjoosten@gmail.com>
2026-07-09 09:09:13 +0200

    ifpkt check

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from lib.check.base import CheckBase
 from lib.check.cpm import CheckCpm
 from lib.check.interface import CheckInterface
 from lib.check.interface_ext import CheckInterfaceExt
+from lib.check.interface_pkt import CheckInterfacePkt
 from lib.check.ip import CheckIp
 from lib.check.ip_address import CheckIpAddress
 from lib.check.lldp import CheckLldp
@@ -19,6 +20,7 @@ if __name__ == '__main__':
         CheckCpm,
         CheckInterface,
         CheckInterfaceExt,
+        CheckInterfacePkt,
         CheckIp,
         CheckIpAddress,
         CheckLldp,
