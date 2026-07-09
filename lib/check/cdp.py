4321cf8a82770c8e9300a01f6e467768382d16b9
Koos85 <koosjoosten@gmail.com>
2026-07-09 22:08:31 +0200

    interfacelookup, remove types without (useful) metrics

--- lib/check/cdp.py
+++ lib/check/cdp.py
@@ -3,12 +3,12 @@ from libprobe.asset import Asset
 from libprobe.check import Check
 from ..snmpclient import get_snmp_client
 from ..snmpquery import snmpquery
+from ..utils import InterfaceLookup
 
 QUERIES = (
+    (MIB_INDEX['IF-MIB']['ifEntry'], True),
     (MIB_INDEX['CISCO-CDP-MIB']['cdpInterfaceEntry'], True),
-    (MIB_INDEX['CISCO-CDP-MIB']['cdpInterfaceExtEntry'], True),
     (MIB_INDEX['CISCO-CDP-MIB']['cdpCacheEntry'], True),
-    (MIB_INDEX['CISCO-CDP-MIB']['cdpCtAddressEntry'], True),
     (MIB_INDEX['CISCO-CDP-MIB']['cdpGlobal'], False),
 )
 
@@ -20,6 +20,39 @@ class CheckCdp(Check):
     async def run(asset: Asset, local_config: dict, config: dict) -> dict:
 
         snmp = get_snmp_client(asset, local_config, config)
-        state = await snmpquery(snmp, QUERIES, True)
 
-        return state
+        if_entry = InterfaceLookup.get(asset.id)
+        if if_entry is None:
+            state_data = await snmpquery(snmp, QUERIES, True)
+            if_entry = InterfaceLookup.set(asset.id, state_data.get('if', []))
+        else:
+            state_data = await snmpquery(snmp, QUERIES[1:], True)
+
+        cdp_if = state_data.get('cdpInterface', [])
+        for item in cdp_if:
+            if_index = item['name']
+
+            try:
+                if_item = if_entry[if_index]
+                item['Interface'] = if_item['Descr']
+            except Exception:
+                continue
+
+        cdp_cache = state_data.get('cdpCache', [])
+        for item in cdp_cache:
+            # split key (ifIndex.deviceIndex)
+            if_index = item['name'].split('.', 1)[0]
+
+            try:
+                if_item = if_entry[if_index]
+                item['Interface'] = if_item['Descr']
+            except Exception:
+                continue
+
+        cdp_global = state_data.get('cdpGlobal', [])
+
+        return {
+            'cdpInterface': cdp_if,
+            'cdpCache': cdp_cache,
+            'cdpGlobal': cdp_global,
+        }
